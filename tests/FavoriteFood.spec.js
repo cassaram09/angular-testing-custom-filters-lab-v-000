d16372533ca41e5d2270b83ab6dd5d15dcfb201a
cassaram09 <cassaram09@gmail.com>
2016-08-05 15:41:48 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,10 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+  it('should filter out only favorite foods', function(){
+    var foods = [{name: 'John', favoriteFood: "pizza"}, {name: 'Emily', favoriteFood: "ice cream"}, {name: 'Mike', favoriteFood: "donuts"} ];
+    var filteredFoods = $filter('favoriteFood')(foods, 'pizza');
+    expect(filteredFoods.length).toEqual(1);
+    expect(filteredFoods).toEqual([{name: 'John', favoriteFood: "pizza"}]);
+  });
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,11 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should filter out all vowels in a string', function(){
+    var string = "Hello my name is Matt and I love pizza";
+    var noVowels = $filter('removeAllVowels')(string);
+    expect(noVowels.length).toEqual(string.length - 12);
+    expect(noVowels).toEqual("Hll my nm s Mtt nd  lv pzz");
+  });
+
 });
